bad08bf2ca99fe18b9103724a84b293b3a3ddfd3
tomasgnzalez <130705700+Tomas6941@users.noreply.github.com>
2026-03-10 22:38:20 -0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("formTurno").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("respuesta").innerText =
-    "Turno enviado correctamente (modo demo).";
-});
